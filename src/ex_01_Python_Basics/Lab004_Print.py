9256756be8468f4accf79c50c85eea5ca8e88b76
Rajveer Singh <rsinghparmar92@gmail.com>
2025-10-11 00:56:46 +0530

    Print function

---
+++ src/ex_01_Python_Basics/Lab004_Print.py
@@ -0,0 +1,5 @@
+print('Hello World')
+#print(*objects, sep=' ', end='\n', file=None, flush=False
+print("Raj",123,3.14,"Parmar",True)
+print("Raj",123,3.14,"Parmar",True, sep='*', end='+++')
+print("Raj",123,3.14,"Parmar",True, sep='*', end='/')
